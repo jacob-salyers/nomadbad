aa93e069a3e6e34fe70fc6040e89af0070f936b6
kswnin <kswnin@caravan-1.lan>
2023-07-22 14:33:17 -0500

    minor bug fixes for flag library

--- server/main.go
+++ server/main.go
@@ -8,13 +8,16 @@ import (
 
 func main() {
 	useSSL := flag.Bool("s", false, "Toggles http/https")
+	flag.Parse()
 	// TODO (jacob): wrap the handler to do templating
 
 	http.Handle("/", logWrapper(http.FileServer(http.Dir("static"))))
 
 	if *useSSL {
+		log.Print("Starting on port 443")
 		log.Fatal(http.ListenAndServeTLS(":443", "/etc/letsencrypt/live/nomad-jiujitsu.com/fullchain.pem", "/etc/letsencrypt/live/nomad-jiujitsu.com/privkey.pem", nil))
 	} else {
+		log.Print("Starting on port 8080")
 		log.Fatal(http.ListenAndServe(":8080", nil))
 	}
 }
