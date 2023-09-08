dfb38870438a9456c377ecf7d9ac05374f6b907a
Jacob Salyers <kswnin@gmail.com>
2023-09-08 18:05:43 -0500

    forgot to add .mjs scripts

---
+++ scripts/calendarToHTML.mjs
@@ -0,0 +1,25 @@
+
+import { promises as fs } from 'fs';
+import { parseICS, generateHTML } from './calendarToHTML-header.mjs';
+
+const schedulePath = process.argv[2] ?? '/dev/stdout';
+const eventsPath = process.argv[3];
+
+process.stdin.setEncoding('utf8');
+
+new Promise((resolve, reject) => {
+	let acc  = '';
+	process.stdin.on('data', data => acc += data);
+	process.stdin.on('error', err => reject(err));
+	process.stdin.on('end', () => resolve(acc));
+})
+	.then(parseICS)
+	.then(generateHTML)
+	.then(events => {
+		const promises = [];
+		promises.push(fs.writeFile(schedulePath, events.recurring));
+		if (eventsPath != null)
+			promises.push(fs.writeFile(eventsPath, events.upcoming));
+
+		return Promise.all(promises);
+	});
