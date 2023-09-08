25aaf1936cff639ae035e1d3646491d3c2589c69
Jacob Salyers <kswnin@gmail.com>
2023-09-08 17:52:16 -0500

    updated generate.sh to pull from the google calendar

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -9,6 +9,9 @@ fi
 d=`pwd`
 cd $CODE_DIR/nomad
 
+curl https://calendar.google.com/calendar/ical/d4d0773546ae6d7e69b104a3f0a6e612ee49bf31fcd87463de373f9819be336c%40group.calendar.google.com/private-1250974c781dffb16a03c7b44773371d/basic.ics \
+	| node scripts/calendarToHTML.mjs > templates/pages/schedule.html
+
 for file in `ls templates/pages`
 do
 sed "
