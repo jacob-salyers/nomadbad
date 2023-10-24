f046debf07b3e21498066976492733cd80ee7ebb
Jacob Salyers <kswnin@gmail.com>
2023-10-23 20:16:27 -0500

    form works!

--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"log"
 	"flag"
 	"net/http"
@@ -12,6 +13,8 @@ func main() {
 	flag.Parse()
 	// TODO (jacob): wrap the handler to do templating
 
+	http.Handle("/api/submit", logWrapper(http.HandlerFunc(apiSubmit)))
+
 	http.Handle("/", logWrapper(http.FileServer(http.Dir("static"))))
 
 	if *useSSL {
@@ -19,8 +22,8 @@ func main() {
 		go redirectToHTTPS()
 		log.Fatal(http.ListenAndServeTLS(":443", "/etc/letsencrypt/live/nomad-jiujitsu.com/fullchain.pem", "/etc/letsencrypt/live/nomad-jiujitsu.com/privkey.pem", nil))
 	} else {
-		log.Print("Starting on port 80")
-		log.Fatal(http.ListenAndServe(":80", nil))
+		log.Print("Starting on port 8080")
+		log.Fatal(http.ListenAndServe(":8080", nil))
 	}
 }
 
@@ -41,4 +44,38 @@ func redirectHelper(res http.ResponseWriter, req *http.Request) {
 
 func redirectToHTTPS() {
 	log.Fatal(http.ListenAndServe(":80", http.HandlerFunc(redirectHelper)))
+}
+
+func apiSubmit(res http.ResponseWriter, req *http.Request) {
+	var message = req.FormValue("message")
+	log.Println(message)
+	f, err := os.OpenFile("./form_submissions.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Print(err)
+		http.ServeFile(res, req, "static/submission_failure.html")
+		return
+	}
+
+	toPrint := fmt.Sprintf(`
+From: %s %s
+Reply-To: %s
+
+%s\n`,
+	req.FormValue("first_name"), req.FormValue("last_name"),
+	req.FormValue("email"), req.FormValue("message"));
+
+
+	if _, err := f.Write([]byte(toPrint)); err != nil {
+		log.Print(err)
+		http.ServeFile(res, req, "static/submission_failure.html")
+		return
+	}
+
+	if err := f.Close(); err != nil {
+		log.Print(err)
+		http.ServeFile(res, req, "static/submission_failure.html")
+		return
+	}
+
+	http.ServeFile(res, req, "static/submission_successful.html")
 }
